daa75b500998d50a21f1c3312f441a99b4ea462c
Eleomar pedro lorenzo <eleomarpedrolorenzo@hotmail.com>
2021-05-30 10:33:15 -0500

    login controller terminated

---
+++ src/controllers/login.js
@@ -0,0 +1,33 @@
+const loginRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+
+loginRouter.post('/', async(req, res) => {
+  const {userName = ' ', password = ''} = req.body;
+
+  const findUser = await User.findOne({userName});
+  const passwordUser = findUser === null
+    ? false
+    : await bcrypt.compare(password, findUser.password);
+
+  if (!(passwordUser && findUser)) {
+    return res.status(401).json({
+      error: 'Invalid user or password'
+    });
+  }
+  const userForToken = {
+    id: findUser._id,
+    user: findUser.userName
+  };
+
+  const token = jwt.sign(userForToken, process.env.WORD_SECRET);
+  res.send({
+    user: findUser.user,
+    token
+  });
+
+});
+
+module.exports = loginRouter;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const userRouter = require('./controllers/user');
 const qualificationRouter = require('./controllers/qualification');
+const loginRouter = require('./controllers/login');
 
 const notFound = require('./middlewares/notFound');
 const handleErros = require('./middlewares/handleErrors');
@@ -19,6 +20,7 @@ app.set('port', process.env.PORT || 4000);
 
 app.use('/api/users', userRouter);
 app.use('/api/qualifications', qualificationRouter);
+app.use('/api/login', loginRouter);
 
 app.use(notFound);
 app.use(handleErros);
