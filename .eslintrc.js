453c58809b7f7e5df1ae03c1635a61746296ac06
Eleomar Pedro Lorenzo <eleomarpedrolorenzo@gmail.com>
2022-12-14 16:02:39 -0600

    refactor: Remove linebreak style

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
   },
   'rules': {
     'indent': ['error', 2],
-    'linebreak-style': ['error', 'windows'],
     'quotes': ['error', 'single'],
     'camelcase': 'error',
     'no-unused-vars': 'warn',
